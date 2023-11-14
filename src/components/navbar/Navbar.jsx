04e9270384d10b3253a48d601f7c131bc6395fb0
QuangNguyen1286 <quang.nguyen@sunbuy.vn>
2023-11-14 11:08:22 +0700

    third

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import styles from "./navbar.module.scss";
+
+const links = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+];
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        My blogs
+      </Link>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className={styles.link}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
