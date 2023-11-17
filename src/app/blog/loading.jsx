06adf6fc1438fb98e17a0ec42b71f8a2e29a91a2
QuangNguyen1286 <quang.nguyen@sunbuy.vn>
2023-11-17 11:35:51 +0700

    eight

---
+++ src/app/blog/loading.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Loading = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>Loading...</h2>
+    </div>
+  );
+};
+
+export default Loading;
